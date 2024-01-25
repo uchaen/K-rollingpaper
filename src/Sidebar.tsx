96ba4c763cc4d7ff1806ad70e26c554883db47ba
uchaen <u_chaen@naver.com>
2024-01-25 15:06:54 +0900

    Style: 메인화면 css 적용

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,17 +1,30 @@
 import "./css/Sidebar.css";
 import { useState } from "react";
+import { IoIosSearch } from "react-icons/io";
 
 function Sidebar() {
-  const [titleList, setTitleList] = useState(["test","test2","te3"]);
+  const [titleList, setTitleList] = useState(["test", "test2", "te3"]);
+  const [selectedTitle, setSelectedTitle] = useState(0);
   return (
     <div className="Sidebar">
-      <div id="mainLogo">K-롤링페이퍼</div>
-      <div id="searchBox">롤링페이퍼 검색</div>
-      {titleList.map((element) => (
-        <div>{element}</div>
+      <div id="mainLogo">K - 롤링페이퍼</div>
+      <div id="searchBox"><IoIosSearch id="searchIcon"/> 롤링페이퍼 검색</div>
+      {titleList.map((element, index) =>
+        selectedTitle === index ? (
+          <div className="titleListElement selectedTitle">{element}</div>
+        ) : (
+          <div
+            className="titleListElement"
+            onClick={() => {
+              setSelectedTitle(index);
+            }}
+          >
+            {element}
+          </div>
+        )
         // <Link to={`/outfitlist/${element.outfitId}`}>{element}</Link>
-      ))}
-      <div>롤링페이퍼 생성 +</div>
+      )}
+      <div className="titleListElement">롤링페이퍼 생성 +</div>
     </div>
   );
 }
