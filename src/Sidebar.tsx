50bb6c6298644015be5e09aa3d35f59780bb0dc8
uchaen <u_chaen@naver.com>
2024-01-30 09:29:00 +0900

    Feat: 검색 기능 구현

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,34 +1,55 @@
 import "./css/Sidebar.css";
-import { useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import Modal from "./Modal";
 import PaperCreate from "./PaperCreate";
 
 function Sidebar() {
-  const [titleList, setTitleList] = useState(["test", "test2", "te3"]);
+  const [fetchTitleList, setFetchTitleList] = useState<string[]>(["test", "test2", "te3"]);
+  const [titleList, setTitleList] = useState<string[]>([]);
   const [isCreateModalOpened, setIsCreateModalOpened] = useState(false);
   const [selectedTitle, setSelectedTitle] = useState(0);
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  const getSearchData = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+
+  useEffect(()=>{
+    setTitleList([...fetchTitleList]);
+  },[fetchTitleList])
+
+  useEffect(() => {
+    if(searchInput != "") {
+      setTitleList(fetchTitleList.filter((element) => element.includes(searchInput)));
+    }
+  }, [searchInput]);
+
   return (
     <div className="Sidebar">
       <div id="mainLogo">K - 롤링페이퍼</div>
       <div id="searchBox">
-        <IoIosSearch id="searchIcon" /> 롤링페이퍼 검색
+        <IoIosSearch id="searchIcon" />
+        <input
+          id="searchText"
+          onChange={getSearchData}
+          type="text"
+          placeholder="롤링페이퍼 검색"
+        />
       </div>
-      {titleList.map(
-        (element, index) =>
-          selectedTitle === index ? (
-            <div className="titleListElement selectedTitle">{element}</div>
-          ) : (
-            <div
-              className="titleListElement"
-              onClick={() => {
-                setSelectedTitle(index);
-              }}
-            >
-              {element}
-            </div>
-          )
-        // <Link to={`/outfitlist/${element.outfitId}`}>{element}</Link>
+      {titleList.map((element, index) =>
+        selectedTitle === index ? (
+          <div className="titleListElement selectedTitle">{element}</div>
+        ) : (
+          <div
+            className="titleListElement"
+            onClick={() => {
+              setSelectedTitle(index);
+            }}
+          >
+            {element}
+          </div>
+        )
       )}
       <div
         className="titleListElement"
