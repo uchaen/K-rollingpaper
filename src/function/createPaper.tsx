e5979d34569743b49cc940284087c08df814226f
uchaen <u_chaen@naver.com>
2024-02-05 13:41:30 +0900

    Fix: paper추가시 타이틀 null인 경우 방지

--- src/function/createPaper.tsx
+++ src/function/createPaper.tsx
@@ -1,16 +1,20 @@
 function createPaper(paperTitle: string, paperPw: string) {
-  fetch(`http://localhost:8080/paper`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      paperTitle: paperTitle,
-      paperPw: paperPw,
-    }),
-  }).then(() => {
-    window.location.reload();
-  });
+  if (paperTitle) {
+    fetch(`http://localhost:8080/paper`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        paperTitle: paperTitle,
+        paperPw: paperPw,
+      }),
+    }).then(() => {
+      window.location.reload();
+    });
+  } else {
+    window.alert("타이틀을 필수로 입력해주세요");
+  }
 }
 
 export default createPaper;
